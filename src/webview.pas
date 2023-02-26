0d51fa39b6e913a0de30d47f09fff9ae6d836811
Pierce Ng <pierce@samadhiweb.com>
2023-02-26 17:24:21 +0800

    Add new APIs. Expand license text.

--- src/webview.pas
+++ src/webview.pas
@@ -1,9 +1,26 @@
-{
- * Copyright (c) 2022 Pierce Ng
- * Published under MIT license.
-}
-
 unit webview;
+
+(*
+ Copyright (c) 2022 Pierce Ng
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*)
 
 {$mode objfpc}{$H+}
 
@@ -45,9 +62,11 @@ function webview_get_window(w: PWebView): Pointer; cdecl; external webview_lib;
 procedure webview_set_title(w: PWebView; const title: PAnsiChar); cdecl; external webview_lib;
 procedure webview_set_size(w: PWebView; width, height, hints: Integer); cdecl; external webview_lib;
 procedure webview_navigate(w: PWebView; const url: PAnsiChar); cdecl; external webview_lib;
+procedure webview_set_html(w: PWebView; const html: PAnsiChar); cdecl; external webview_lib;
 procedure webview_init(w: PWebView; const js: PAnsiChar); cdecl; external webview_lib;
 procedure webview_eval(w: PWebView; const js: PAnsiChar); cdecl; external webview_lib;
 procedure webview_bind(w: PWebView; const name: PAnsiChar; fn: TWebViewBindProc; arg: Pointer); cdecl; external webview_lib;
+procedure webview_unbind(w: PWebView; const name: PAnsiChar); cdecl; external webview_lib;
 procedure webview_return(w: PWebView; const seq: PAnsiChar; status: Integer; const result: PAnsiChar); cdecl; external webview_lib;
 
 implementation
