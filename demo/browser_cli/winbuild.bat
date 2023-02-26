3d5bd13dfa054c511b11f5d30bbb7476d85b7b41
Pierce Ng <pierce@samadhiweb.com>
2023-02-26 18:09:27 +0800

    Rewrite winbuild.bat to expect fpc.exe on PATH. Closes #7

--- demo/browser_cli/winbuild.bat
+++ demo/browser_cli/winbuild.bat
@@ -1,16 +1,10 @@
 @echo off
-
-echo Set up FPC executable path.
-set fpcexe=c:fpc.exe
-if not exist "%fpcexe%" (
-	echo ERROR: Edit this batch file to set up location of fpc.exe
-	exit /b 1
-)
-echo "%fpcexe%"
-
 echo Building...
-copy "..\..\dll\x86_64\libwebview.a" .
+copy "..\..\dll\x86_64\libimpwebview.a" .
 copy "..\..\dll\x86_64\webview.dll" .
 copy "..\..\dll\x86_64\WebView2Loader.dll" .
-"%fpcexe%" -Fu..\..\src -Fl. browser_cli.lpr
-
+fpc.exe -Twin64 -Px86_64 -Fu..\..\src -Fl. browser_cli.lpr
+if errorlevel 1 (
+	echo ERROR: not found fpc.exe
+	echo ERROR: Edit this batch file to set up location of fpc.exe
+)
--- demo/js_bidir/winbuild.bat
+++ demo/js_bidir/winbuild.bat
@@ -3,9 +3,8 @@ echo Building...
 copy "..\..\dll\x86_64\libimpwebview.a" .
 copy "..\..\dll\x86_64\webview.dll" .
 copy "..\..\dll\x86_64\WebView2Loader.dll" .
-fpc.exe -Twin64 -Px86_64 -Fu..\..\src -Fl. servcli.lpr
+fpc.exe -Twin64 -Px86_64 -Fu..\..\src -Fl. js_bidir.lpr
 if errorlevel 1 (
 	echo ERROR: not found fpc.exe
 	echo ERROR: Edit this batch file to set up location of fpc.exe
 )
-
